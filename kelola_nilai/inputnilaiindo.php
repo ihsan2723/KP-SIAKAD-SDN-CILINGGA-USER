b400b8edc9512c50c34c9046c7f056f0f09f1fe2
ihsan2723 <m.ihsanpnd@gmail.com>
2024-06-14 21:31:05 +0700

    mengganti sidebar

--- kelola_nilai/inputnilaiindo.php
+++ kelola_nilai/inputnilaiindo.php
@@ -5,12 +5,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard Admin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/tampilnilai1-6.css">
 </head>
 
@@ -27,28 +24,20 @@
                         <a href="../dashboard.php">Dashboard</a>
                     </li>
                     <li class="button">
-                        <i class="bi bi-house-add-fill"></i>
-                        <a href="../profil_sekolah/profilsekolah.php">Profil Sekolah</a>
-                    </li>
-                    <li class="button">
-                        <i class="bi bi-person-plus-fill"></i>
-                        <a href="guru.php">Guru</a>
-                    </li>
-                    <li class="button">
-                        <i class="bi bi-book-half"></i>
-                        <a href="../mapel/mapel.php" class="feat-btn">Mata Pelajaran</a>
-                    </li>
-                    <li class="button">
-                        <i class="i bi-people-fill"></i>
-                        <a href="../siswa/siswa.php">Siswa</a>
-                    </li>
-                    <li class="button">
                         <i class="fa-solid fa-clipboard-user"></i>
-                        <a href="../presensi/presensi.php" style="margin-left: 8px">Presensi</a>
+                        <a href="../presensi/presensi.php" style="margin-left: 4px">Presensi</a>
+                    </li>
+                    <li class="button">
+                        <i class="bi bi-clipboard2-check-fill"></i>
+                        <a href="../rekap_presensi/rekap_presensi.php">Rekap Presensi</a>
                     </li>
                     <li class="button">
                         <i class="fa-solid fa-list-check"></i>
-                        <a href="../nilai/nilai.php">Nilai</a>
+                        <a href="../kelola_nilai/kelolanilai.php">Kelola Nilai</a>
+                    </li>
+                    <li class="button">
+                        <i class="bi bi-person-lines-fill"></i>
+                        <a href="../biodata_guru/biodata_guru.php" class="feat-btn">Biodata Guru</a>
                     </li>
                 </ul>
             </div>
@@ -92,16 +81,13 @@
                                     <td>Widia N</td>
                                     <td>9/30</td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td></td>
                                 </tr>
@@ -111,16 +97,13 @@
                                     <td>Della D</td>
                                     <td>10/30</td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td></td>
                                 </tr>
@@ -130,23 +113,19 @@
                                     <td>M Ihsan</td>
                                     <td>9/30</td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td>
-                                        <input class="input" type="text"
-                                            style="background-color: #FEFAE0; border:none; width:30px;">
+                                        <input class="input" type="text" style="background-color: #FEFAE0; border:none; width:30px;">
                                     </td>
                                     <td> </td>
                                 </tr>
                             </table>
                             <div class="simpan">
-                                <button class="bg-success text-light"
-                                    style="padding: 7px 30px; border:none; float:right;">Simpan</button>
+                                <button class="bg-success text-light" style="padding: 7px 30px; border:none; float:right;">Simpan</button>
                             </div>
                         </div>
                     </div>
@@ -160,12 +139,12 @@
         </div>
     </div>
     <script>
-    document.querySelectorAll('.feat-btn').forEach((item) => {
-        item.addEventListener('click', (event) => {
-            const parent = item.parentElement;
-            parent.classList.toggle('active');
+        document.querySelectorAll('.feat-btn').forEach((item) => {
+            item.addEventListener('click', (event) => {
+                const parent = item.parentElement;
+                parent.classList.toggle('active');
+            });
         });
-    });
     </script>
 </body>
 
