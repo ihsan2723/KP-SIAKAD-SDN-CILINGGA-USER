9212c7b4c54383f79d5a6e2af93dbce5a95abd15
ihsan2723 <m.ihsanpnd@gmail.com>
2024-06-14 22:35:06 +0700

    membuat tampilan biodata guru

---
+++ biodata_guru/biodata_guru.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/biodata.css" />
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="sidebar">
+                <header>
+                    <img src="../gambar/logosd.png" alt="" width="100" height="100" />
+                </header>
+                <ul>
+                    <li class="button">
+                        <i class="bi bi-grid-fill"></i>
+                        <a href="../dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="button">
+                        <i class="fa-solid fa-clipboard-user"></i>
+                        <a href="../presensi/presensi.php" style="margin-left: 4px">Presensi</a>
+                    </li>
+                    <li class="button">
+                        <i class="bi bi-clipboard2-check-fill"></i>
+                        <a href="../rekap_presensi/rekap_presensi.php">Rekap Presensi</a>
+                    </li>
+                    <li class="button">
+                        <i class="fa-solid fa-list-check"></i>
+                        <a href="../kelola_nilai/kelolanilai.php">Kelola Nilai</a>
+                    </li>
+                    <li class="button">
+                        <i class="bi bi-person-lines-fill"></i>
+                        <a href="../biodata_guru/biodata_guru.php" class="feat-btn">Biodata Guru</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="container">
+                <nav class="navbar">
+                    <div id="indikator">
+                        <a href="kelolanilai.php" class="bi bi-arrow-left"></a>
+                        <h5>Halaman Biodata Guru</h5>
+                    </div>
+                    <ul>
+                        <li>Admin |</li>
+                        <li>Widia Nurindri</li>
+                        <li id="logout">
+                            <i class="bi bi-box-arrow-right"></i>
+                            <a id="out" href="../login.php">Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+                <hr />
+                <div class="box">
+                    <div class="container">
+                        <div>
+                            <h1></h1>
+                        </div>
+                        <div class="profile-card">
+                            <div class="profile-image">
+                                <img src="../gambar/logosd.png" alt="Profile Picture">
+                            </div>
+                            <div class="profile-info">
+                                <h1>John Doe</h1>
+                                <p><strong>No Induk:</strong> 0011</p>
+                                <p><strong>Nama Guru:</strong> Widia Nurindri</p>
+                                <p><strong>Kedudukan:</strong> PNS</p>
+                                <p><strong>Alamat:</strong> Majalengka</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <footer>
+                <h3 class="text-light">CopyRight 2024_Kelompok 2</h3>
+            </footer>
+        </div>
+    </div>
+    <script>
+    document.querySelectorAll('.feat-btn').forEach((item) => {
+        item.addEventListener('click', (event) => {
+            const parent = item.parentElement;
+            parent.classList.toggle('active');
+        });
+    });
+    </script>
+</body>
+
+</html>
